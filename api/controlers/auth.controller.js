d16583d8799c6a2f85b9315585c8eeee250d86a9
Mafath <ahamedmafath550@gmail.com>
2024-12-24 17:52:08 +0530

    complete sign in page functionality

--- api/controlers/auth.controller.js
+++ api/controlers/auth.controller.js
@@ -20,16 +20,28 @@ export const signup = async (req,res, next) => { //next use krnne middlewear
 };
 
 export const signin = async(req,res,next) => {
-  const {email,password} = req.body;
+  const {email,password} = req.body; //user input box wla ghn ewa const 2kt save krgnnwa
   try {
-    const validUser = await User.findOne({email: email});
-    if(!validUser) return next(errorHandler(404, 'User not found'));
+                          //Here we use our 'User' model
+                               //findOne is a method to find some info in mongo db
+                              //model eke email: api hdpu const email
+    const validUser = await User.findOne({email: email}); //check whether email exists or not (mehi email kyl 1 prk dmmth athi). If emial exists, we save that user in a const called 'validUser'
+    if(!validUser) return next(errorHandler(404, 'User not found')); //if email does not exist. (here we use the custom error we created)
 
-    const validPassword = bcryptjs.compareSync(password,validUser.password);
-    if(!validPassword) return next(errorHandler(401, 'Wrong credentials'));
+    //api body eken gnne normal password ekk. But the password inside the database is a hashed one. So we use 'compareSync' method
+    const validPassword = bcryptjs.compareSync(password,validUser.password);//we compare both passwords together
+    if(!validPassword) return next(errorHandler(401, 'Wrong credentials'));//if password is not correct
 
+    //if both email and password is correct,
+    //we need to authenticate the user. The way we do the authentication is by adding a cookie inside the browser
+    //We need to create a hash token which includes the email or the id of the user, and then we save this token inside the browser cookie
+    //Whenever user wants to change email or password we have to check whether the user is authenticated or not. We can use that cookie for that
+    //But here we not gonna save the data as it is. We gonna hash that data as well. Best package for that is JWT. We use this package to create the token
     const token = jwt.sign({id: validUser._id}, process.env.JWT_SECRET);
 
+    //after creating the token, we wanna save it as the cookie
+
+    //but lets remove the password from the valid user first
     const {password: pass, ...rest} = validUser._doc;
 
     // after creating the token we wanna save this token as  the cookie
@@ -37,13 +49,14 @@ export const signin = async(req,res,next) => {
 
     //expire wenna one nttn(so this is a session)
     res
-      .cookie('access_token', token, {httpOnly: true})
+      .cookie('access_token', token, {httpOnly: true}) // kmthi namak denna cookie ekt('acess_token').This cookie include user id. But it is hashed. | http:true kynne no other 3rd party application can have access to our cookie
       .status(200)
-      .json(rest);
+      .json(rest);//we send back the user without the password
     //So now we have the cookie inside the browser
 
   }
   catch (error) {
+    //catch the error using middlewear
     next(error);
   }
 };
--- client/src/pages/SignIn.jsx
+++ client/src/pages/SignIn.jsx
@@ -1,9 +1,74 @@
-import React from 'react'
+import React, { useState } from 'react'
+import { Link, useNavigate } from 'react-router-dom'
 
-export default function Signin() {
+export default function SignIn() {
+
+  const [formData, setFormData] = useState({});
+  //store any error messages if backend returns an error
+  const [error, setError] = useState(null);
+  //This tracks whether the form is in the process of submitting, so you can disable the button and show a loading indicator.
+  const [loading,setLoading] = useState(false);
+  //This hook is used to navigate to different pages in our app after the form is submitted
+  const navigate = useNavigate();
+
+  const handleChange = (e) => {
+    setFormData({...formData, [e.target.id]: e.target.value});
+  };
+
+  const handleSubmit = async (e) => {
+    e.preventDefault(); // to prevent refreshing the page when we click sign up
+
+  try {
+    setLoading(true);
+
+    //Sends a POST request to our backend (/api/auth/signup) with the form data.
+    const res = await fetch('/api/auth/signin', {
+      method: 'POST',
+      headers: {
+        'Content-Type' : 'application/json',//telling the backend that the request body contains JSON data.
+      },
+      body: JSON.stringify(formData),//formData object is stringified
+    });
+
+    //The response from the backend is converted to JSON
+    //data refers to the result or the response that we get from the backend after sending the request
+    const data = await res.json();
+    console.log(data);
+    
+
+    //If the response indicates an error,
+    if(data.success === false) {//middleware eken output wena 'success' ek gana kynne
+      setLoading(false);
+      setError(data.message);
+      return;
+    }
+    setLoading(false);
+    setError(null);
+    navigate('/');
+  }
+  catch (error) {
+    setLoading(false);
+    setError(data.message);    
+  }
+  };
+
+
   return (
-    <div>
-      SignIn
+    <div className='p-3 max-w-lg mx-auto'>
+      <h1 className='text-3xl text-center font-semibold my-7'>Sign In</h1>
+      <form onSubmit={handleSubmit} className='flex flex-col gap-4'>
+        <input type='email' placeholder='email' className='border p-3 rounded-lg' id='email' onChange={handleChange}/>
+        <input type='password' placeholder='password' className='border p-3 rounded-lg' id='password' onChange={handleChange}/>
+        {/* loading true nm disable wenwa */}
+        <button disabled={loading} className='bg-slate-700 text-white p-3 rounded-lg uppercase hover:opacity-95 disabled:opacity-80'>{loading ? 'Loading...' : 'Sign In'}</button>
+      </form>
+      <div className='flex gap-2 mt-5'>
+        <p>Dont have an account?</p>
+        <Link to='/sign-up'>
+          <span className='text-blue-700'>Sign up</span>
+        </Link>
+      </div>
+      {error && <p className='text-red-500 mt-5'>{error}</p>}
     </div>
   )
 }
